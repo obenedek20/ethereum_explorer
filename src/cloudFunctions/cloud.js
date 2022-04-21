f499ac0172524ec78dd55480a2161313aa374649
oscarbenedek <oscarbenedek@gmail.com>
2022-04-20 20:20:59 -0700

    cloud

--- src/cloudFunctions/cloud.js
+++ src/cloudFunctions/cloud.js
@@ -8,5 +8,13 @@ Moralis.Cloud.define("searchEthAddress", async (request) => {
 });
 
 Moralis.Cloud.define("getEthTransactions", async(request) => {
-  //PLACEHOLDER
+    const query1 = new Moralis.Query("EthTransactions");
+    //request.params.address holds adress we want to search for
+    query1.equalTo("from_address", request.params.address);
+    const query2 = new Moralis.Query("EthTransactions");
+    query2.equalTo("to_address", request.params.address);
+    //holds all transactions holding address
+    const combinedQuery = new Moralis.Query.or(query1, query2);
+    combinedQuery.descending("block_number");
+    combinedQuery.withCount();
 });
